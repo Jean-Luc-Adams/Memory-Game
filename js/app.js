527c9728197c7c3c5fdc288b8c5310d1920faedd
Jean-Luc-Adams <jladams91@gmail.com>
2018-07-27 12:45:06 -0500

    Added restart button and move counter

--- js/app.js
+++ js/app.js
@@ -9,59 +9,96 @@ const listOfCards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "f
  *   - add each card's HTML to the page
  */
 
- const cardsContainer = document.querySelector(".deck");
+const cardsContainer = document.querySelector(".deck");
 
- let openedCards = [];
- let matchedCards = [];
+let openedCards = [];
+let matchedCards = [];
 
-// Loop through and create each card
- for(let i = 0; i < listOfCards.length; i++) {
-     const card = document.createElement("div");
-     card.classList.add("card");
-     card.innerHTML = "<i class='" + listOfCards[i] + "'</i>";
-     cardsContainer.appendChild(card);
+// Start the game!!!
 
-    // Create card click event
-     card.addEventListener("click", function () {
+function startGame() {
+    // Loop through and create each card
+    for (let i = 0; i < listOfCards.length; i++) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = "<i class='" + listOfCards[i] + "'</i>";
+        cardsContainer.appendChild(card);
+        // Initialize the click function
+        click(card);
+    }
+}
 
-        const currentCard = this;
+
+// Create card click event
+// Made it's own function for easier readiablity 
+function click(card) {
+    card.addEventListener("click", function () {
+        const firstCard = this;
         const lastCard = openedCards[0];
-
-        if(openedCards.length === 1) {
-
-            card.classList.add("open", "show");
+        if (openedCards.length === 1) {
+            card.classList.add("open", "show", "disabled");
             openedCards.push(this);
-
             // Match check
-            if(currentCard.innerHTML === lastCard.innerHTML) {
-               currentCard.classList.add('match');
-               lastCard.classList.add('match');
-               // "Resets the conditional for additional matches"
-               matchedCards.push(currentCard, lastCard);
-               openedCards = [];
-               // Check for game over
-               gameOver();
-            } else {
-                currentCard.classList.remove("open", "show");
-                lastCard.classList.remove("open", "show");
-                // Reset the conditional for additional non-matches
-                openedCards = [];
-            }
+            compareCards(firstCard, lastCard);
         } else {
-            currentCard.classList.add("open", "show");
+            firstCard.classList.add("open", "show", "disabled");
             openedCards.push(this);
-           
         }
-      
     })
- }
+}
 
+// Compare cards
+function compareCards(firstCard, lastCard) {
+    if (firstCard.innerHTML === lastCard.innerHTML) {
+        firstCard.classList.add('match');
+        lastCard.classList.add('match');
+        // "Resets the conditional for additional matches"
+        matchedCards.push(firstCard, lastCard);
+        openedCards = [];
+        // Check for game over
+        gameOver();
+    } else {
+        //When not matching, gives a 3/4 of a  sec timeframe before resetting
+        setTimeout(function () {
+            firstCard.classList.remove("open", "show", "disabled");
+            lastCard.classList.remove("open", "show", "disabled");
+            // Reset the conditional for additional non-matches
+            openedCards = [];
+        }, 750);
+    }
+    // Increase the move counter per match or mismatch
+    moveCounter();
+}
 
+// Is the game over??
+function gameOver() {
+    if (matchedCards.length === listOfCards.length) {
+        alert("GOOD JOB!");
+    }
+}
 
+// Move counter 
+const moveNumber = document.querySelector(".moves");
+let moves = 0; 
+function moveCounter() {
+    moves++;
+    moveNumber.innerHTML = moves;
+}
+
+//Wanna try again???
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener('click', function() {
+    cardsContainer.innerHTML = "";
+    startGame();
+    matchedCards =  [];
+})
+
+startGame();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle(listOfCards) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -74,9 +111,6 @@ function shuffle(array) {
     return array;
 }
 
-
-
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -87,10 +121,3 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-function gameOver() {
-    if(matchedCards.length === listOfCards.length) {
-        alert("GOOD JOB!");
-    }
-}
-
